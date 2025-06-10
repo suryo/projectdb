50b5b3376f9a8fba37108cf9ad5055330f9f9b3c
suryo <suryoatm@gmail.com>
2025-06-10 21:14:56 +0700

    create order controller

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        echo "Displaying all orders";
+        $isitableorder = DB::select('SELECT * FROM orders');
+        dd($isitableorder);
+        //return view('orders.index');
+    }
+
+    public function summary()
+    {
+        $summary = DB::select('SELECT * FROM order_summary');
+        dd($summary);
+        return view('summary', ['summary' => $summary]);
+    }
+}
